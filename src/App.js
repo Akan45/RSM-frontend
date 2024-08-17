52cb934fe4451c20e16c96748df8e2a3b0a46872
Abhinash04 <abhinash.pritiraj@gmail.com>
2024-08-17 23:47:48 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import "bootstrap/dist/css/bootstrap.css";
+import "./styles.css";
+import SignIn from "./components/SignIn";
+import Register from "./components/Register";
+import Profile from "./pages/Profile";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Layout name={name} />}>
+        <Route path="" element={<Home />} />
+        <Route path="register" element={<Register />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route
+          path="profile"
+          element={
+            localStorage.getItem("authToken") ? (
+              <Profile setName={setName} />
+            ) : (
+              <Navigate to="/signin" />
+            )
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
 
